853ca2c8cb3e9ca7bc7696a2c063aa20863f2807
Dan Ryan Neff <danryanneff.reads@gmail.com>
2026-05-31 22:28:59 -0500

    Build success: private demo + robots/sitemap/index fixes

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,14 +2,13 @@ export const prerender = true;
 
 export function GET({ request }) {
 const origin = new URL(request.url).origin;
-const lines = [
-"User-agent: *",
-"Allow: /",
-"Disallow: /preview/",
-"Disallow: /demos/",
-Sitemap: ${origin}/sitemap.xml,
-];
-return new Response(lines.join("\n"), {
+const body =
+"User-agent: *\n" +
+"Allow: /\n" +
+"Disallow: /preview/\n" +
+"Disallow: /demos/\n" +
+"Sitemap: " + origin + "/sitemap.xml";
+return new Response(body, {
 headers: { "Content-Type": "text/plain; charset=utf-8" },
 });
 }
